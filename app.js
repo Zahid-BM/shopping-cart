6c359088c57987375e3a9679f3ff3ae26d6206aa
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-02-14 19:56:23 +0600

    phone qtty add click plus button handler done

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 document.getElementById('btn-phone-add').addEventListener('click', function () {
-    console.log('function success');
+    const getInput = document.getElementById('input-phone-qtty');
+    const getInputValue = getInput.value;
+    const getInputValueNumber = parseInt(getInputValue);
+    const getInputQtty = getInputValueNumber + 1;
+    getInput.value = getInputQtty
 })
