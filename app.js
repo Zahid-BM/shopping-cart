1b3dee69ba3dc2a3747d9fe90f3d7bca31a542a8
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-02-15 09:04:59 +0600

    phoneQttyPrice() function, add, decrease and price update done

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-document.getElementById('btn-phone-add').addEventListener('click', function () {
+
+function phoneQttyPrice(isIncrease) {
     const getInput = document.getElementById('input-phone-qtty');
     const getInputValue = getInput.value;
-    const getInputValueNumber = parseInt(getInputValue);
-    const getInputQtty = getInputValueNumber + 1;
-    getInput.value = getInputQtty
+    let getInputValueNumber = parseInt(getInputValue);
+    if (isIncrease == true) {
+        getInputValueNumber = getInputValueNumber + 1;
+        getInput.value = getInputValueNumber
+    }
+    else if (getInputValueNumber > 0) {
+        getInputValueNumber = getInputValueNumber - 1;
+        getInput.value = getInputValueNumber
+    }
+    const getPhonePrice = document.getElementById('phone-price');
+    const phonePriceInnerText = getPhonePrice.innerText;
+    getPhonePrice.innerText = 1219 * getInputValueNumber;
+}
+// event handler for btn-phone-add 
+document.getElementById('btn-phone-add').addEventListener('click', function () {
+    phoneQttyPrice(true);
+})
+// event handler for btn-phone-deduct
+document.getElementById('btn-phone-deduct').addEventListener('click', function () {
+    phoneQttyPrice(false);
 })
